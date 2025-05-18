c58c03f769efa14c116abb6d014508f58a8a2867
hazemsalah55 <omar.boss2840@gmail.com>
2025-05-18 15:30:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# برنامج بسيط بيحسب مجموع رقمين
+
+def main():
+    print("مرحباً بك في برنامج جمع رقمين!")
+    num1 = float(input("ادخل الرقم الأول: "))
+    num2 = float(input("ادخل الرقم الثاني: "))
+    result = num1 + num2
+    print("المجموع هو:", result)
+
+if __name__ == "__main__":
+    main()
+
